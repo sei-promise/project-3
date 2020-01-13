e332cc66457bdb9738c02d1551c5f8bf7288f1da
Mohammed Al-Mohana <m.mohana2022@gmail.com>
2020-01-13 12:31:08 +0300

    add axios

---
+++ src/components/Questions.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import axios from "axios"
+
+class Questions extends Component {
+    state = { 
+        
+     }
+    render() { 
+        return ( 
+            <div>
+
+            </div>
+         );
+    }
+}
+ 
+export default Questions;
