0584fe4bbfe93f30b6618ef8f5a87dbd0632f027
Mohammed Al-Mohana <m.mohana2022@gmail.com>
2020-01-15 16:44:39 +0300

    Updates

---
+++ precache-manifest.56a0256fd99861e680a86b0947932d26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5764c0614dd4b0f5525eff3f7891ec",
+    "url": "/project-3/index.html"
+  },
+  {
+    "revision": "ad2a9f27af48c7dec138",
+    "url": "/project-3/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3b3562c2af4f0633913e",
+    "url": "/project-3/static/css/main.786a81b9.chunk.css"
+  },
+  {
+    "revision": "ad2a9f27af48c7dec138",
+    "url": "/project-3/static/js/2.3c0a1f52.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/project-3/static/js/2.3c0a1f52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b3562c2af4f0633913e",
+    "url": "/project-3/static/js/main.c6eb6977.chunk.js"
+  },
+  {
+    "revision": "19fa69ea6adf3f5a1234",
+    "url": "/project-3/static/js/runtime-main.a35eb810.js"
+  },
+  {
+    "revision": "125ef13957b2d103c28f77ebb804daf4",
+    "url": "/project-3/static/media/BookBG.125ef139.jpg"
+  }
+]);
